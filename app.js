1226cd7192e1387f96e3ee610235748b89c75749
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-06-01 19:16:29 -0700

    js-import-export

---
+++ app.js
@@ -0,0 +1,8 @@
+// let user=prompt("Enter your name")
+
+import {add,mul,division,sub} from "./math.js";
+
+console.log(add(2,2))
+console.log(mul(2,2))
+console.log(sub(2,2))
+console.log(division(2,2))
---
+++ math.js
@@ -0,0 +1,18 @@
+function mul(x,y){
+    return x * y
+}
+
+function sub(x,y){
+    return x - y
+}
+
+function division(x,y){
+    return x / y
+}
+
+function add(x,y){
+    return x + y
+}
+export {
+    mul,sub,division,add
+} 
